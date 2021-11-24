69f571e4924d425f423e755c8faab365a36a1288
Ibrahim Ali <59145840+ByIbrahimAli@users.noreply.github.com>
2021-11-24 20:06:04 +0000

    serde json feature now implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::collections::HashMap;
-use std::io::Read;
-use std::str::FromStr;
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please, specify an action you sexy specimen");
@@ -41,27 +39,22 @@ struct Todo {
 
 impl Todo {
     // reads content of db.txt and gives us back our Todo populated with previous stored value
-    fn new() -> Result<Todo, std::io::Error> {
-    // new function returns either a Todo struct or an io:Error
-
-        let mut f = std::fs::OpenOptions::new() // configures how to open db.txt
+    fn new() -> Result<Todo, std::io::Error> { // new function returns either a Todo struct or an io:Error
+        // open db.json
+        let f = std::fs::OpenOptions::new()
             .write(true)
-            .create(true)// creates db.txt if not already present
+            .create(true)
             .read(true)
-            .open("db.txt")?;
+            .open("db.json")?; // file extension db.json instead of db.txt
 
-        let mut content = String::new();
-
-        f.read_to_string(&mut content)?; // reads all bytes in file and appends them into the content String
-
-        let map: HashMap<String, bool> = content // converts from String type of db.txt to HashMap by binding a map variable
-            .lines()// creates iterator over each line of a string
-            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>()) // splits lines on tab char
-            .map(|v| (v[0], v[1]))// collect::<Vec<&str>>() transforms an iterator into a relevant collection then transform into tuple using .map(v[0], v[1]))
-            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))// Convert the two elements of tuple into a String and a boolean
-            .collect(); // Collect the above into our HashMap
-
-        Ok(Todo { map }) // If no errors return struct to caller with Ok(Todo { map })
+        // seralize json as HashMap
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }), // deserializes file for us
+            Err(e) if e.is_eof() => Ok(Todo { // Match guard. If file empty, return empty HashMap.
+                map: HashMap::new(),
+            }),
+            Err(e) => panic!("An error occurred: {}", e), // All other errors, exit immediately
+        }
     }
 
     fn insert(&mut self, key: String) {
@@ -70,19 +63,16 @@ impl Todo {
         self.map.insert(key, true);
     }
 
-    fn save(self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k, v) in self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record)
-        }
-        std::fs::write("db.txt", content)
+    fn save(self) -> Result<(), Box <dyn std::error::Error>> { // Box returns pointer to error rather than error itself so caller can handle it. This is because it may return a file system error when opening file or a serde error when converting it, we don't know which it'll return
+        // open db.json
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("db.json")?;
 
-        // -> annotates the returned type from the function. We are returning a Result.
-        // We iterate over the map, and format each string, separating key and value with a tab
-        //  character and each line with a new line.
-        // We push the formatted string into a content variable.
-        // We write content inside a file called db.txt.
+        // write to file with serde
+        serde_json::to_writer_pretty(f, &self.map)?; // writes our hashmap as a JSON
+        Ok(())
     }
 
     fn complete(&mut self, key: &String) -> Option<()> { // function return type: empty Option
